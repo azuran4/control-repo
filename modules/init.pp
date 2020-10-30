9ed3d1fea7acd5309e814dec455ff8272621dd43
azuran4 <73542113+azuran4@users.noreply.github.com>
2020-10-30 17:53:30 +0300

    Create init.pp

---
+++ modules/init.pp
@@ -0,0 +1,33 @@
+class mediawiki {
+  $phpmysql = $osfamily ? {
+    'redhat' => 'php-mysql',
+    'debian' => 'php5-mysql',
+    default => 'php5-mysql',
+    }
+   
+   package { $phpmysql:
+    ensure => 'present';
+   }
+   
+   if $osfamily == 'redhat' {
+    package { 'php-xml':
+      ensure => 'present',
+    }
+   }
+   
+   class { '::apache':
+    docroot => '/var/www/html'
+    mpm_module => 'prefork',
+    subscribe => Package[$phpmysql],
+   }
+    
+   class { '::apache::mod::php':}
+   
+   vcsrepo {' /var/www/html':
+      ensure => present,
+      provider => git,
+      source => 'https://github.com/wikimedia/mediawiki.git',
+      revision => 'REL1_23',
+   }
+   
+ }
