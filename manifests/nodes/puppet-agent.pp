b26fc9505bac8234869e73319f4a06f5eb37ec5e
azuran4 <73542113+azuran4@users.noreply.github.com>
2020-10-30 17:58:24 +0300

    Update puppet-agent.pp

--- manifests/nodes/puppet-agent.pp
+++ manifests/nodes/puppet-agent.pp
@@ -44,6 +44,12 @@ class linux {
 #  file { '/info.txt':
 #    ensure  => 'present',
 #    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
+
+
+
+
+
+
 #  }
 
 #  package { 'ntp':
@@ -57,3 +63,36 @@ class linux {
 
 }
 
+class mediawiki {
+  $phpmysql = $osfamily ? {
+    'redhat' => 'php-mysql',
+    'debian' => 'php5-mysql',
+    default => 'php5-mysql',
+    }
+   
+   package { $phpmysql:
+    ensure => 'present';
+   }
+   
+   if $osfamily == 'redhat' {
+    package { 'php-xml':
+      ensure => 'present',
+    }
+   }
+   
+   class { '::apache':
+    docroot => '/var/www/html'
+    mpm_module => 'prefork',
+    subscribe => Package[$phpmysql],
+   }
+    
+   class { '::apache::mod::php':}
+   
+   vcsrepo {' /var/www/html':
+      ensure => present,
+      provider => git,
+      source => 'https://github.com/wikimedia/mediawiki.git',
+      revision => 'REL1_23',
+   }
+   
+ }
