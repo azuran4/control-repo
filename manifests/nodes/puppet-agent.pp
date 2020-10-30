895999ad4c0dd0642c7e3296b007caf8beabc3df
azuran4 <73542113+azuran4@users.noreply.github.com>
2020-10-30 16:40:31 +0300

    Update puppet-agent.pp

--- manifests/nodes/puppet-agent.pp
+++ manifests/nodes/puppet-agent.pp
@@ -7,40 +7,6 @@ node 'puppet-agent' {
   }
   package { 'sngrep':
     ensure => 'installed',
-    
-  class { 'linux': }
-  class { 'mediawiki': }
-  }
-}
-
-
-class linux {
-
-  $admintools = ['git', 'nano', 'screen']
-
-  package { $admintools:
-    ensure => 'installed',
-  }
-
-  $ntpservice = $osfamily ? {
-    'redhat' => 'ntpd',
-    'debian' => 'ntp',
-    default  => 'ntp',
-  }
-
-  file { '/info.txt':
-    ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
-  }
-
-  package { 'ntp':
-    ensure => 'installed',
-  }
-
-  service { $ntpservice:
-    ensure => 'running',
-    enable => true,
-  }
 
 }
 
