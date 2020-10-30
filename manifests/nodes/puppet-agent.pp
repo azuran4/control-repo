4c43571298f055faf023436a0ba981d40b267d9a
azuran4 <73542113+azuran4@users.noreply.github.com>
2020-10-30 18:07:36 +0300

    Update puppet-agent.pp

--- manifests/nodes/puppet-agent.pp
+++ manifests/nodes/puppet-agent.pp
@@ -67,7 +67,7 @@ class mediawiki {
   $phpmysql = $osfamily ? {
     'redhat' => 'php-mysql',
     'debian' => 'php5-mysql',
-    default => 'php5-mysql',
+    default => 'php-mysql',
     }
    
    package { $phpmysql:
