421ff847ef2d1ba6c553b39e5d8bc49b1d78a676
azuran4 <73542113+azuran4@users.noreply.github.com>
2020-11-02 18:38:47 +0300

    Update puppet-agent.pp

--- manifests/nodes/puppet-agent.pp
+++ manifests/nodes/puppet-agent.pp
@@ -124,7 +124,7 @@ class mediawiki {
    file { 'LocalSettings.php':
        path    => '/var/www/html/LocalSettings.php',
        ensure  => 'file',
-       content => template('mediawiki/LocalSettings.erb'),
+       content => template('::mediawiki/LocalSettings.erb'),
    }
 
 }
